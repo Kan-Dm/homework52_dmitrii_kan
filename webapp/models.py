598ecc3eca8aafc4c931e849397ad29f6de64482
Dmitrii <funmaker04@gmail.com>
2023-11-27 23:31:10 +0600

    добавил путь к представлению index_view в файлу urls.py, также подкорректировал status в классе Task

--- webapp/models.py
+++ webapp/models.py
@@ -5,7 +5,7 @@ status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),
 
 class Task(models.Model):
     description = models.CharField(max_length=50, verbose_name="Описание")
-    status = models.CharField(max_length=10, default='Новая', verbose_name="Статус", choices=status_choices)
+    status = models.CharField(max_length=20, default='Новая', verbose_name="Статус", choices=status_choices)
     due_date = models.DateField(verbose_name='Дата выполнения', null=True, blank=True)
 
     def __str__(self):
