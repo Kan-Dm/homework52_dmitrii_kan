4eb58edc940227afabcfec31f92d59acdd9c1a86
Dmitrii <funmaker04@gmail.com>
2023-11-28 01:18:30 +0600

    добавил путь к представлению create_task_view, также подредактировал HTML файл task_create.html

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from webapp.views import index_view
+from webapp.views import index_view, create_task_view
 
 urlpatterns = [
-    path('', index_view)
+    path('', index_view),
+    path('task/add/', create_task_view)
 ]
