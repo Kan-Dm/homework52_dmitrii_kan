0bfdb25f2ae795831e3af9529faf1d66f0c497b4
Dmitrii <funmaker04@gmail.com>
2023-11-27 23:13:26 +0600

    добавил модель Task в файл models.py

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
+
+
+class Task(models.Model):
+    description = models.CharField(max_length=50, verbose_name="Описание")
+    status = models.CharField(max_length=10, default='Новая', verbose_name="Статус", choices=status_choices)
+    due_date = models.DateField(verbose_name='Дата выполнения', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.id}. {self.description}'
