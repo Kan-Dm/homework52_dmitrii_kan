dc865e3585f423ce7f4d2eb8eb9bb7a80ef578ab
Dmitrii <funmaker04@gmail.com>
2023-11-27 23:56:35 +0600

    добавил в админку модель Task

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from webapp.models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'due_date']
+    list_display_links = ['id', 'description']
+    list_filter = ['status']
+    search_fields = ['description', 'status']
+    fields = ['description', 'status', 'due_date']
+    readonly_fields = ['due_date']
