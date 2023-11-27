be802f7989ff98ca1a4c75ada7114cac4a69e0c3
Dmitrii <funmaker04@gmail.com>
2023-11-28 00:46:29 +0600

    добавил представление create_task_view для отображения страницы создания нового задания, также подкорректировал HTML файл task_create.html

--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from webapp.models import Task
+from webapp.models import Task, status_choices
 
 
 def index_view(request):
     tasks = Task.objects.all()
-    return render(request, template_name='index.html', context={'tasks' : tasks})
+    return render(request, template_name='index.html', context={'tasks': tasks})
+
+
+def create_task_view(request):
+    return render(request, template_name='task_create.html', context={'status_choices': status_choices})
