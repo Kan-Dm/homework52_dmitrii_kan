b7c125f0c18a8baa4eda899e8f1f027cee874906
Dmitrii <funmaker04@gmail.com>
2023-11-30 08:53:36 +0600

    Добавил в адимнку поле detailed_description для отображения

--- webapp/admin.py
+++ webapp/admin.py
@@ -4,8 +4,8 @@ from webapp.models import Task
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['id', 'description', 'status', 'due_date']
+    list_display = ['id', 'description', 'status', 'due_date', 'detailed_description']
     list_display_links = ['id', 'description']
     list_filter = ['status']
     search_fields = ['description', 'status']
-    fields = ['description', 'status', 'due_date']
+    fields = ['description', 'status', 'due_date', 'detailed_description']
