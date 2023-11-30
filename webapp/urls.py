266272b29f684a1aafcd42a30d4bd7244ff01c4a
Dmitrii <funmaker04@gmail.com>
2023-11-30 12:22:46 +0600

    Именовал пути до файлов html для того чтобы в дальнейшем обращаться к файлам не по пути, а по названию

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from webapp.views import index_view, create_task_view, delete_task_view, task_view
 
 urlpatterns = [
-    path('', index_view),
-    path('task/add/', create_task_view),
-    path('task/delete/<int:pk>', delete_task_view),
-    path('task/<int:pk>', task_view)
+    path('', index_view, name='index'),
+    path('task/add/', create_task_view, name='task_add'),
+    path('task/delete/<int:pk>', delete_task_view, name='task_delete'),
+    path('task/<int:pk>', task_view, name='task_view')
 ]
