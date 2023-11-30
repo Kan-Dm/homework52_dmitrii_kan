8b356530f47671753480404958c9a97f06df9085
Dmitrii <funmaker04@gmail.com>
2023-11-30 14:08:57 +0600

    Импортировал шорткат redirect и переписал логику редиректа. Также переписал файл task_create.html, добавил бутстрап и поле для добавления детального описания задачи

--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from webapp.models import Task, status_choices
-from django.http import HttpResponseRedirect
 
 
 def index_view(request):
@@ -8,7 +7,7 @@ def index_view(request):
     return render(request, template_name='index.html', context={'tasks': tasks})
 
 
-def create_task_view(request):
+def create_task_view(request, *args, **kwargs):
     if request.method == 'GET':
         return render(request, template_name='task_create.html', context={'status_choices': status_choices})
     elif request.method == 'POST':
@@ -16,17 +15,18 @@ def create_task_view(request):
             date = None
         else:
             date = request.POST.get('due_date')
-        Task.objects.create(
+        task = Task.objects.create(
             description=request.POST.get('description'),
             status=request.POST.get('status'),
-            due_date=date
+            due_date=date,
+            detailed_description=request.POST.get('detailed_description')
         )
-        return HttpResponseRedirect('/')
+        return redirect('task_view', pk=task.pk)
 
 
 def delete_task_view(request, pk):
     Task.objects.filter(pk=pk).delete()
-    return HttpResponseRedirect('/')
+    return redirect('index')
 
 
 def task_view(request, pk):
