1720aca58954932f881c7b3beaf387c7b72432e3
Dmitrii <funmaker04@gmail.com>
2023-12-09 22:17:40 +0600

    В файле urls.py добавил путь до представления редактирования задачи.
    Подредактировал task_edit.html, передавался неверный ключ.
    
    В index.html добавил ссылку на страницу редактирования, также отделил ссылки редактирования и удаления задачи перенеся их на другую строку, чтобы ссылка на подробную информацию находилась первой.

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from webapp.views import index_view, create_task_view, delete_task_view, task_view
+from webapp.views import index_view, create_task_view, delete_task_view, task_view, task_edit_view
 
 urlpatterns = [
     path('', index_view, name='index'),
     path('task/add/', create_task_view, name='task_add'),
-    path('task/delete/<int:pk>', delete_task_view, name='task_delete'),
-    path('task/<int:pk>', task_view, name='task_view')
+    path('task/delete/<int:pk>/', delete_task_view, name='task_delete'),
+    path('task/<int:pk>/', task_view, name='task_view'),
+    path('task/edit/<int:pk>/', task_edit_view, name='task_edit')
 ]
